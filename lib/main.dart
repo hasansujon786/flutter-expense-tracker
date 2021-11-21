0219e2a8beca58ef84f7da370fa8928f5585cca6
hasansujon786 <hasansujon786@gmail.com>
2021-11-22 02:28:32 +0600

    add bottom sheet

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:expense_tracker/widgets/user_transactions.dart';
+import 'package:expense_tracker/widgets/transaction_list.dart';
+import 'package:expense_tracker/widgets/new_transaction.dart';
+import 'package:expense_tracker/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,9 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'EXT'),
     );
   }
 }
@@ -23,11 +25,74 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  void showTransactionInputs(BuildContext context) {
+    showModalBottomSheet(
+      backgroundColor: Colors.transparent,
+      context: context,
+      isScrollControlled: true,
+      isDismissible: true,
+      builder: (BuildContext bottomSheetContext) {
+        return FractionallySizedBox(
+          heightFactor: 0.8,
+          child: Container(
+            padding: const EdgeInsets.only(top: 24),
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+              color: Colors.grey.shade100,
+            ),
+            child: Column(
+              /* mainAxisAlignment: MainAxisAlignment.start, */
+              /* mainAxisSize: MainAxisSize.min, */
+              children: <Widget>[
+                const Text(
+                  'Create New Transaction',
+                  style: TextStyle(fontSize: 22),
+                ),
+                NewTransaction(
+                  addNewTransaction: _addNewTransaction,
+                  onCloseModalBottomSheet: () => Navigator.pop(context),
+                ),
+                /* ElevatedButton( */
+                /*   child: const Text('Close BottomSheet'), */
+                /*   onPressed: () => Navigator.pop(context), */
+                /* ) */
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  final List<Transaction> _userTransactions = [
+    Transaction(
+        id: 't1', title: 'new show', amount: 70.0, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Weekly Groceries', amount: 50.0, date: DateTime.now())
+  ];
+
+  void _addNewTransaction(String title, double amount) {
+    final newTx = Transaction(
+      id: DateTime.now().toString(),
+      date: DateTime.now(),
+      title: title,
+      amount: amount,
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,13 +112,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 100,
               ),
             ),
-            const UserTransactions(),
+            TransactionList(
+              transactions: _userTransactions,
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Increment',
+        onPressed: () {
+          showTransactionInputs(context);
+        },
+        tooltip: 'Create transaction',
         child: const Icon(Icons.add),
       ),
     );
--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
-  NewTransaction({Key? key, required this.addNewTransaction}) : super(key: key);
+class NewTransaction extends StatefulWidget {
+  const NewTransaction(
+      {Key? key,
+      required this.addNewTransaction,
+      required this.onCloseModalBottomSheet})
+      : super(key: key);
   final Function addNewTransaction;
+  final Function onCloseModalBottomSheet;
 
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
   final titleConroller = TextEditingController();
   final amountConroller = TextEditingController();
 
@@ -13,42 +23,40 @@ class NewTransaction extends StatelessWidget {
     if (title.isEmpty || amount <= 0) {
       return;
     }
-    addNewTransaction(title, amount);
+    widget.addNewTransaction(title, amount);
+    widget.onCloseModalBottomSheet();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'What is the reason of your expanse?',
-              ),
-              controller: titleConroller,
-              onSubmitted: (_) => submitData(),
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'What is the reason of your expanse?',
             ),
-            const SizedBox(height: 8),
-            TextField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'What is the amount of your expanse?',
-              ),
-              controller: amountConroller,
-              keyboardType:
-                  const TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) => submitData(),
+            controller: titleConroller,
+            onSubmitted: (_) => submitData(),
+          ),
+          const SizedBox(height: 8),
+          TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'What is the amount of your expanse?',
             ),
-            TextButton(
-              child: const Text('Add Transaction'),
-              onPressed: submitData,
-            )
-          ],
-        ),
+            controller: amountConroller,
+            keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            onSubmitted: (_) => submitData(),
+          ),
+          TextButton(
+            child: const Text('Add Transaction'),
+            onPressed: submitData,
+          )
+        ],
       ),
     );
   }
